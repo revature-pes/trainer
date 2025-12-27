2b7c02a1b39a2453d44bd569277726fd327ed562
revature2025 <nithin.n.c@revature.com>
2025-12-27 15:37:17 +0530

    object5 and 6 programs added

--- week2/js_programs/object5.js
+++ week2/js_programs/object5.js
@@ -12,3 +12,4 @@ const var2 = trainer.tech(); // Calling the method and storing the return value 
 console.log(var2 + "   " + typeof(var2)); // Prints:  5  number
 var1();  //PRINTS HUGE AMOUNT OF INFO, when the program is run using CLI node.  WHATS THAT???
 num = var1();
+console.log(num + "   " + typeof(num));
---
+++ week2/js_programs/object6.js
@@ -0,0 +1,22 @@
+const student = {
+	name : 'mani',
+	techStudent() {
+		console.log(this);
+	}
+};
+
+const trainer = {
+	name : 'Nithin',
+	techTrainer() {
+		console.log(this);
+	}
+};
+
+const walk1 = trainer.techTrainer.bind(student);
+walk1();
+
+const walk2 = trainer.techTrainer;
+walk2();
+
+const walk3 = trainer.techTrai.bind(trainer);
+walk3();
